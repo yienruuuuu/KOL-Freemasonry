6879fef79a2af18432e62ad381e10ce2d4b2445a
Eric.Lee <a23034@athena.com.tw>
2025-04-23 16:44:27 +0800

    配置selenium

--- kol-freemasonry-twitter/src/main/java/io/github/yienruuuuu/impl/SeleniumServiceImpl.java
+++ kol-freemasonry-twitter/src/main/java/io/github/yienruuuuu/impl/SeleniumServiceImpl.java
@@ -1,5 +1,6 @@
 package io.github.yienruuuuu.impl;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.yienruuuuu.SeleniumService;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,6 +19,11 @@ public class SeleniumServiceImpl implements SeleniumService {
     @Value("${webdriver.chrome.path}")
     private String chromeDriverPath;
 
+    /**
+     * 獲取WebDriver實例，
+     * 因應未來程式運行在docker上，但headless chrome容易被反bot的情況下，
+     * 支持headed模式下連上預先開啟的chrome。
+     */
     @Override
     public WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
